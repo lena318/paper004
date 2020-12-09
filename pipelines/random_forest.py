28778162fa20c8db929b6599ca3a4ec7fdf201ee
Lena Armstrong <56137845+lena318@users.noreply.github.com>
2020-12-09 12:57:00 -0500

    Add files via upload

--- pipelines/random_forest.py
+++ pipelines/random_forest.py
@@ -18,123 +18,35 @@ Output: Random Forest predictions of FC based on SC
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
-
-import pickle
-
 import numpy as np
 import pandas as pd
 
+from python_files.testing_python_toolbox import electrode_row_and_column_names
 
-def FC_SC_random_forest(file_directory, sub_ID_array, HUP_ID, random_atlases, standard_atlases, perm_list,
-                        start_preictal, end_preictal, start_ictal, end_ictal, features, FC_list):
 
-    # Get electrode localization
-    electrode_localization_by_atlas_file_paths = []
-    for s in sub_ID_array:
-        for ra in random_atlases:
-            for p in range(len(perm_list)):
-                if p < 9:
-                    file = '{0}sub-{1}/electrode_localization/electrode_localization_by_atlas/sub-{2}_electrode_' \
-                           'coordinates_mni_{3}_Perm000{4}.csv'.format(
-                        file_directory, s, s, ra, perm_list[p])
-                else:
-                    file = '{0}sub-{1}/electrode_localization/electrode_localization_by_atlas/sub-{2}_electrode_' \
-                           'coordinates_mni_{3}_Perm00{4}.csv'.format(
-                        file_directory, s, s, ra, perm_list[p])
-                electrode_localization_by_atlas_file_paths.append(file)
-        for sa in standard_atlases:
-            file = '{0}sub-{1}/electrode_localization/electrode_localization_by_atlas/sub-{2}_electrode_coordinates_' \
-                   'mni_{3}.csv'.format(file_directory, s, s, sa)
-            electrode_localization_by_atlas_file_paths.append(file)
-
-    # Accesses files for functional connectivity matrices (preictal)
-    FC_preictal_file_path_array = []
-    for x in range(len(standard_atlases)):
-        for s in range(len(sub_ID_array)):
-            file = '{0}sub-{1}/connectivity_matrices/functional/eeg/sub-{2}_{3}_{4}_{5}_functionalConnectivity.pickle' \
-                .format(file_directory, sub_ID_array[s], sub_ID_array[s], HUP_ID[s], start_preictal[s], end_preictal[s])
-            FC_preictal_file_path_array.append(file)
-    print(len(FC_preictal_file_path_array))
-
-    # Accesses files for functional connectivity matrices (ictal)
-    FC_ictal_file_path_array = []
-    for x in range(len(standard_atlases)):
-        for s in range(len(sub_ID_array)):
-            file = '{0}sub-{1}/connectivity_matrices/functional/eeg/sub-{2}_{3}_{4}_{5}_functionalConnectivity.pickle'\
-                .format(file_directory, sub_ID_array[s], sub_ID_array[s], HUP_ID[s], start_ictal[s], end_ictal[s])
-            FC_ictal_file_path_array.append(file)
-    print(len(FC_ictal_file_path_array))
-
-    # Get functional connectivity data in pickle file format (preictal)
-    FC_preictal_list = []
-    for FC_file_path in FC_preictal_file_path_array:
-        with open(FC_file_path, 'rb') as f: broadband, alphatheta, beta, lowgamma, highgamma, \
-                                    electrode_row_and_column_names, order_of_matrices_in_pickle_file = pickle.load(f)
-        FC_preictal_list.append([broadband])
-
-    # Get functional connectivity data in pickle file format (ictal)
-    FC_ictal_list = []
-    for FC_file_path in FC_ictal_file_path_array:
-        with open(FC_file_path, 'rb') as f: broadband, alphatheta, beta, lowgamma, highgamma, \
-                                    electrode_row_and_column_names, order_of_matrices_in_pickle_file = pickle.load(f)
-        FC_ictal_list.append([broadband])
+def FC_SC_random_forest(features, FC_preictal_list, FC_ictal_list, FC_list,
+                        electrode_localization_by_atlas_file_paths):
 
     # average preictal
-    FC_average_preictal = []
-    for i in range(len(FC_preictal_list)):
-        row = []
-        for j in range(len(FC_preictal_list[i])):
-            col = []
-            for x in range(len(FC_preictal_list[i][j])):
-                col.append(0)
-            row.append(col)
-        FC_average_preictal.append(row)
+    FC_average_preictal = np.zeros(shape=(len(FC_preictal_list), len(FC_preictal_list[0]), len(FC_preictal_list[0][0])))
+    print(FC_average_preictal.shape)
 
-    for i in range(len(FC_preictal_list)):
-        for j in range(len(FC_preictal_list[i])):
-            for x in range(len(FC_preictal_list[i][j])):
-                FC_average_preictal[i][j][x] += FC_preictal_list[i][j][x]
-
-    for i in range(len(FC_preictal_list)):
-        for j in range(len(FC_preictal_list[i])):
-            for x in range(len(FC_preictal_list[i][j])):
-                FC_average_preictal[i][j][x] /= len(FC_preictal_list[i][j])
+    for file in len(FC_average_preictal):
+        for row in len(FC_average_preictal[file]):
+            for col in len(FC_average_preictal[row]):
+                FC_average_preictal[row, col] = np.meannaremove(FC_preictal_list[file, row, col, :])
 
     # average ictal
-    FC_average_ictal = []
-    for i in range(len(FC_ictal_list)):
-        row_ictal = []
-        for j in range(len(FC_ictal_list[i])):
-            col_ictal = []
-            for x in range(len(FC_ictal_list[i][j])):
-                col_ictal.append(0)
-            row_ictal.append(col_ictal)
-        FC_average_ictal.append(row_ictal)
+    FC_average_ictal = np.zeros(shape=(len(FC_ictal_list), len(FC_ictal_list[0]), len(FC_ictal_list[0][0])))
+    print(FC_average_ictal.shape)
 
-    for i in range(len(FC_ictal_list)):
-        for j in range(len(FC_preictal_list[i])):
-            for x in range(len(FC_preictal_list[i][j])):
-                FC_average_ictal[i][j][x] += FC_ictal_list[i][j][x]
-
-    for i in range(len(FC_ictal_list)):
-        for j in range(len(FC_ictal_list[i])):
-            for x in range(len(FC_ictal_list[i][j])):
-                FC_average_ictal[i][j][x] /= len(FC_ictal_list[i][j])
+    for file in len(FC_average_ictal):
+        for row in len(FC_average_ictal[file]):
+            for col in len(FC_average_ictal[row]):
+                FC_average_preictal[row, col] = np.meannaremove(FC_ictal_list[file, row, col, :])
 
     # subtract preictal - ictal
-    for i in range(len(FC_preictal_list)):
-        row = []
-        for j in range(len(FC_preictal_list[i])):
-            col = []
-            for x in range(len(FC_preictal_list[i][j])):
-                col.append(0)
-            row.append(col)
-        FC_list.append(row)
-
-    for i in range(len(FC_ictal_list)):
-        for j in range(len(FC_ictal_list[i])):
-            for x in range(len(FC_ictal_list[i][j])):
-                FC_list[i][j][x] = FC_average_preictal[i][j][x] - FC_average_ictal[i][j][x]
+    FC_list = FC_ictal_list - FC_preictal_list #???
 
     # Get electrode localization by atlas csv file data. From get_electrode_localization.py
     electrode_localization_by_atlas = []
@@ -143,82 +55,82 @@ def FC_SC_random_forest(file_directory, sub_ID_array, HUP_ID, random_atlases, st
 
     # Remove electrodes in "electrode localization" not found in Functional Connectivity matrices
     electrode_localization_names = []
-    for i in range(len(electrode_localization_by_atlas)):
-        electrode_localization_names.append(np.array(electrode_localization_by_atlas[i]['electrode_name']))
-        electrode_localization_by_atlas[i] = electrode_localization_by_atlas[i][
-            np.in1d(electrode_localization_names[i], electrode_row_and_column_names)]
+    for file in range(len(electrode_localization_by_atlas)):
+        electrode_localization_names.append(np.array(electrode_localization_by_atlas[file]['electrode_name']))
+        electrode_localization_by_atlas[file] = electrode_localization_by_atlas[file][
+            np.in1d(electrode_localization_names[file], electrode_row_and_column_names)]
 
     # Remove electrodes in the Functional Connectivity matrices not found in "electrode localization"
-    for x in range(len(electrode_localization_names)):
+    for file in range(len(electrode_localization_names)):
         not_in_functional_connectivity = np.in1d(electrode_row_and_column_names, electrode_localization_names)
         for i in range(len(FC_list)):
             for j in range(len(FC_list[i])):
-                FC_list[i][j] = FC_list[i][j][not_in_functional_connectivity, :, :]
-                FC_list[i][j] = FC_list[i][j][:, not_in_functional_connectivity, :]
+                FC_list[file][i] = FC_list[file][i][not_in_functional_connectivity, :, :]
+                FC_list[file][i] = FC_list[file][i][:, not_in_functional_connectivity, :]
 
     # Fisher z-transform of functional connectivity data. This is to take means of correlations and do correlations to
     # the structural connectivity
-    for i in range(len(FC_list)):
-        for j in range(len(FC_list[i])):
-            FC_list[i][j] = np.arctanh(FC_list[i][j])
+    for file in range(len(FC_list)):
+        for i in range(len(FC_list[file])):
+            FC_list[file][i] = np.arctanh(FC_list[file][i])
 
     # Remove structural ROIs not in electrode_localization ROIs
     electrode_ROIs = []
     structural_index = []
-    for i in range(len(electrode_localization_by_atlas)):
-        electrode_ROIs.append(np.unique(np.array(electrode_localization_by_atlas[i].iloc[:, 4])))
-        electrode_ROIs[i] = electrode_ROIs[i][~(electrode_ROIs[i] == 0)]  # remove region 0
-        structural_index.append(np.array(electrode_ROIs[i] - 1))  # subtract 1 because of python's zero indexing
-    for j in range(len(features)):
-        for x in range(len(features[j])):
-            features[j][x] = features[j][x][structural_index[j], :]
-            features[j][x] = features[j][x][:, structural_index[j]]
+    for file in range(len(electrode_localization_by_atlas)):
+        electrode_ROIs.append(np.unique(np.array(electrode_localization_by_atlas[file].iloc[:, 4])))
+        electrode_ROIs[file] = electrode_ROIs[file][~(electrode_ROIs[file] == 0)]  # remove region 0
+        structural_index.append(np.array(electrode_ROIs[file] - 1))  # subtract 1 because of python's zero indexing
+    for row in range(len(features)):
+        for col in range(len(features[row])):
+            features[row][col] = features[row][col][structural_index[row], :]
+            features[row][col] = features[row][col][:, structural_index[row]]
 
     # Taking average functional connectivity for those electrodes in same atlas regions
     ROIs = []
-    for i in range(len(FC_list)):
+    for file in range(len(FC_list)):
         print('length FC_List', len(FC_list))
-        for j in range(len(FC_list[i])):
-            print('length i FC_List', len(FC_list[i]))
-            ROIs.append(np.array(electrode_localization_by_atlas[i].iloc[:, 4]))
-            for r in range(len(electrode_ROIs[i])):
-                index_logical = (ROIs[i] == electrode_ROIs[i][r])
+        for i in range(len(FC_list[file])):
+            print('length i FC_List', len(FC_list[file]))
+            ROIs.append(np.array(electrode_localization_by_atlas[file].iloc[:, 4]))
+            for r in range(len(electrode_ROIs[file])):
+                index_logical = (ROIs[file] == electrode_ROIs[file][r])
                 index_first = np.where(index_logical)[0][0]
                 index_second_to_end = np.where(index_logical)[0][1:]
-                mean = np.mean(FC_list[i][j][index_logical, :, :], axis=0)
+                mean = np.mean(FC_list[file][i][index_logical, :, :], axis=0)
                 # Fill in with mean.
-                FC_list[i][j][index_first, :, :] = mean
-                FC_list[i][j][:, index_first, :] = mean
+                FC_list[file][i][index_first, :, :] = mean
+                FC_list[file][i][:, index_first, :] = mean
                 # delete the other rows and columns belonging to same region.
-                FC_list[i][j] = np.delete(FC_list[i][j], index_second_to_end, axis=0)
-                FC_list[i][j] = np.delete(FC_list[i][j], index_second_to_end, axis=1)
+                FC_list[file][i] = np.delete(FC_list[file][i], index_second_to_end, axis=0)
+                FC_list[file][i] = np.delete(FC_list[file][i], index_second_to_end, axis=1)
                 # keeping track of which electrode labels correspond to which rows and columns
-                ROIs[i] = np.delete(ROIs[i], index_second_to_end, axis=0)
+                ROIs[file] = np.delete(ROIs[file], index_second_to_end, axis=0)
             # remove electrodes in the ROI labels as zero
-            index_logical = (ROIs[i] == 0)
+            index_logical = (ROIs[file] == 0)
             index = np.where(index_logical)[0]
-            FC_list[i][j] = np.delete(FC_list[i][j], index, axis=0)
-            FC_list[i][j] = np.delete(FC_list[i][j], index, axis=1)
-            ROIs[i] = np.delete(ROIs, index, axis=0)
+            FC_list[file][i] = np.delete(FC_list[file][i], index, axis=0)
+            FC_list[file][i] = np.delete(FC_list[file][i], index, axis=1)
+            ROIs[file] = np.delete(ROIs, index, axis=0)
 
     # break down feature matrix 2D - 1D FC matrix !!!
     features_2D = []
-    for i in range(len(features)):
+    for row in range(len(features)):
         all_depth = []
-        for col in range(len(features[i])):
-            for j in range(len(features[i])):  # Depth of 9
-                for x in range(len(features[i][j])):
-                    all_depth.append(features[i][col][x])
+        for col in range(len(features[row])):
+            for depth in range(len(features[row])):  # Depth of 9
+                for x in range(len(features[row][col])):
+                    all_depth.append(features[row][col][depth])
                 features_2D.append(all_depth)
 
     print("length features: ", len(features_2D))
     # print(features_2D)
 
     labels_1D = []
-    for i in range(len(FC_list)):
-        for j in range(len(FC_list[i])):
-            for x in range(len(FC_list[i][j])):
-                labels_1D.append(FC_list[i][j][x]) # only use broadband now
+    for file in range(len(FC_list)):
+        for row in range(len(FC_list[file])):
+            for col in range(len(FC_list[file][row])):
+                labels_1D.append(FC_list[file][row][col])  # only use broadband now
 
     print("length labels: ", len(labels_1D))
     # print(labels_1D)
--- pipelines/random_forest_script.py
+++ pipelines/random_forest_script.py
@@ -9,7 +9,7 @@ Purpose:
 Logic of code:
     1. Creates file directory
     2. Creates array of sub_IDs, array of HUP_IDs, array of random atlases, array of standard atlases,
-    list of permutations, array of starting ictal times, array of ending ictal times
+    list of permutations, arrays of starting preictal and ictal times, arrays of ending preictal ictal times
     3. Creates a feature matrix for each file
     4. Created an emtpy FC list
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -21,9 +21,9 @@ Output: Random Forest predictions of FC based on SC
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
-
-import random_forest
-from create_feature_matrix import create_feature_matrix
+import pickle
+from python_files import random_forest
+from python_files.create_feature_matrix import create_feature_matrix
 
 # 7 patients, 24 atlases (11 random atlases with 30 permutations & 13 standard atlases)
 # File stats: 2,401 files - 2,310 random atlases (330 per patient) & 91 standard atlases (13 per patient)
@@ -145,7 +145,7 @@ end_ictal.append(458615820000)  # RID0278
 #end_ictal.append(250847964650)  # RID0536
 #end_ictal.append(286885777902)  # RID0536
 
-file_directory1 = '/Users/larmstrong2020/mount/DATA/Human_Data/BIDS_processed/'
+file_directory = '/Users/larmstrong2020/mount/DATA/Human_Data/BIDS_processed/'
 
 # Creates features, which are structural connectivity matrices
 features = []
@@ -154,19 +154,66 @@ for s in sub_ID_array:
         for p in range(len(perm_list)):
             if p < 9:
                 file = '{0}sub-{1}/connectivity_matrices/structural/{2}/sub-{3}_ses-preop3T_dwi-eddyMotionB0Corrected' \
-                       '.nii.gz.trk.gz.{4}_Perm000{5}.count.pass.connectivity.mat'.format(file_directory1, s, ra, s, ra,
+                       '.nii.gz.trk.gz.{4}_Perm000{5}.count.pass.connectivity.mat'.format(file_directory, s, ra, s, ra,
                                                                                           perm_list[p])
             else:
                 file = '{0}sub-{1}/connectivity_matrices/structural/{2}/sub-{3}_ses-preop3T_dwi-eddyMotionB0Corrected' \
-                       '.nii.gz.trk.gz.{4}_Perm00{5}.count.pass.connectivity.mat'.format(file_directory1, s, ra, s, ra,
+                       '.nii.gz.trk.gz.{4}_Perm00{5}.count.pass.connectivity.mat'.format(file_directory, s, ra, s, ra,
                                                                                          perm_list[p])
             features.append(create_feature_matrix(file))
     for sa in standard_atlases:
         file = '{0}sub-{1}/connectivity_matrices/structural/{2}/sub-{3}_ses-preop3T_dwi-eddyMotionB0Corrected.nii.gz.' \
-               'trk.gz.{4}.count.pass.connectivity.mat'.format(file_directory1, s, sa, s, sa)
+               'trk.gz.{4}.count.pass.connectivity.mat'.format(file_directory, s, sa, s, sa)
         features.append(create_feature_matrix(file))
+
+# Get electrode localization
+electrode_localization_by_atlas_file_paths = []
+for s in sub_ID_array:
+    for ra in random_atlases:
+        for p in range(len(perm_list)):
+            if p < 9:
+                file = '{0}sub-{1}/electrode_localization/electrode_localization_by_atlas/sub-{2}_electrode_' \
+                           'coordinates_mni_{3}_Perm000{4}.csv'.format(file_directory, s, s, ra, perm_list[p])
+            else:
+                file = '{0}sub-{1}/electrode_localization/electrode_localization_by_atlas/sub-{2}_electrode_' \
+                           'coordinates_mni_{3}_Perm00{4}.csv'.format(file_directory, s, s, ra, perm_list[p])
+            electrode_localization_by_atlas_file_paths.append(file)
+    for sa in standard_atlases:
+        file = '{0}sub-{1}/electrode_localization/electrode_localization_by_atlas/sub-{2}_electrode_coordinates_' \
+                   'mni_{3}.csv'.format(file_directory, s, s, sa)
+        electrode_localization_by_atlas_file_paths.append(file)
+
+# Accesses files for functional connectivity matrices (preictal)
+FC_preictal_file_path_array = []
+for x in range(len(standard_atlases)):
+    for s in range(len(sub_ID_array)):
+        file = '{0}sub-{1}/connectivity_matrices/functional/eeg/sub-{2}_{3}_{4}_{5}_functionalConnectivity.pickle' \
+            .format(file_directory, sub_ID_array[s], sub_ID_array[s], HUP_ID[s], start_preictal[s], end_preictal[s])
+        FC_preictal_file_path_array.append(file)
+
+# Accesses files for functional connectivity matrices (ictal)
+FC_ictal_file_path_array = []
+for x in range(len(standard_atlases)):
+    for s in range(len(sub_ID_array)):
+        file = '{0}sub-{1}/connectivity_matrices/functional/eeg/sub-{2}_{3}_{4}_{5}_functionalConnectivity.pickle'\
+                .format(file_directory, sub_ID_array[s], sub_ID_array[s], HUP_ID[s], start_ictal[s], end_ictal[s])
+        FC_ictal_file_path_array.append(file)
+
+# Get functional connectivity data in pickle file format (preictal)
+FC_preictal_list = []
+for FC_file_path in FC_preictal_file_path_array:
+    with open(FC_file_path, 'rb') as f: broadband, alphatheta, beta, lowgamma, highgamma, \
+                                    electrode_row_and_column_names, order_of_matrices_in_pickle_file = pickle.load(f)
+    FC_preictal_list.append([broadband])
+
+# Get functional connectivity data in pickle file format (ictal)
+FC_ictal_list = []
+for FC_file_path in FC_ictal_file_path_array:
+    with open(FC_file_path, 'rb') as f: broadband, alphatheta, beta, lowgamma, highgamma, \
+                                    electrode_row_and_column_names, order_of_matrices_in_pickle_file = pickle.load(f)
+    FC_ictal_list.append([broadband])
 
 FC_list = []
 
-random_forest.FC_SC_random_forest(file_directory1, sub_ID_array, HUP_ID, [], standard_atlases, [], start_ictal,
-                                  end_ictal, features, FC_list)
+random_forest.FC_SC_random_forest(features, FC_preictal_list, FC_ictal_list, FC_list,
+                                  electrode_localization_by_atlas_file_paths)
